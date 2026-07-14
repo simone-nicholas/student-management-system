8c369ea20ed881002bfd973f13f09eadc1115253
Waldren56 <nicholassimone10@gmail.com>
2026-07-14 23:45:31 +0200

    test: add getAllStudents controller test

---
+++ src/main/java/com/company/studentmanagementsystem/books/BookFinder.java
@@ -0,0 +1,22 @@
+package com.company.studentmanagementsystem;
+
+import com.company.studentmanagementsystem.books.BookRepository;
+import com.company.studentmanagementsystem.books.model.Book;
+import com.company.studentmanagementsystem.exceptions.BookNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class BookFinder {
+    private final BookRepository bookRepository;
+
+    public BookFinder(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public Book getBookById(Long id) {
+        return bookRepository.findById(id)
+                .orElseThrow(() -> new BookNotFoundException(id));
+    }
+}
---
+++ src/main/java/com/company/studentmanagementsystem/courses/CourseFinder.java
@@ -0,0 +1,4 @@
+package com.company.studentmanagementsystem.courses;
+
+public class CourseFinder {
+}
